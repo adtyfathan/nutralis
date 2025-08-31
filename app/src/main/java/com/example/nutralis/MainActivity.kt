1aebb968ee5b6cb831c5a5c39d3179dbe2f03c80
adtyfathan <adtyfathan04@gmail.com>
2025-08-31 15:33:01 +0700

    develop auth ui

--- app/src/main/java/com/example/nutralis/MainActivity.kt
+++ app/src/main/java/com/example/nutralis/MainActivity.kt
@@ -4,42 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.ExitToApp
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.ExitToApp
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.*
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.*
 import androidx.navigation.navArgument
 import com.example.nutralis.navigation.Screen
-import com.example.nutralis.ui.auth.AuthViewModel
-import com.example.nutralis.ui.auth.LoginScreen
-import com.example.nutralis.ui.auth.RegisterScreen
+import com.example.nutralis.ui.auth.*
 import com.example.nutralis.ui.home.HomeScreen
-import com.example.nutralis.ui.product.ProductInputScreen
-import com.example.nutralis.ui.product.ProductResultScreen
-import com.example.nutralis.ui.product.ProductScanScreen
-import com.example.nutralis.ui.product.ScannedProductScreen
+import com.example.nutralis.ui.product.*
 import com.example.nutralis.ui.theme.NutralisTheme
 import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
@@ -53,68 +35,52 @@ class MainActivity : ComponentActivity() {
         setContent {
             NutralisTheme {
                 val navController = rememberNavController()
-                val bottomScreen = listOf(Screen.Home, Screen.Scan, Screen.Input, Screen.Scanned)
+                val bottomScreens = listOf(Screen.Home, Screen.Scan, Screen.Input, Screen.Scanned)
                 val auth = FirebaseAuth.getInstance()
                 val authViewModel: AuthViewModel = hiltViewModel()
 
-                val navBackStackEntry by navController.currentBackStackEntryAsState()
-                val currentRoute = navBackStackEntry?.destination?.route.orEmpty()
-
-                val showBars = currentRoute != Screen.Login.route &&
-                        currentRoute != Screen.Register.route
+                val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route.orEmpty()
+                val showBars = currentRoute !in listOf(Screen.Login.route, Screen.Register.route)
 
                 Scaffold(
                     topBar = {
-                        if (showBars){
+                        if (showBars) {
                             CenterAlignedTopAppBar(
                                 colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
                                     containerColor = MaterialTheme.colorScheme.primaryContainer,
                                     titleContentColor = MaterialTheme.colorScheme.primary
                                 ),
                                 title = {
-                                    Text(
-                                        "Nutralis",
-                                        maxLines = 1,
-                                        overflow = TextOverflow.Ellipsis
-                                    )
+                                    Text("Nutralis", maxLines = 1, overflow = TextOverflow.Ellipsis)
                                 },
                                 actions = {
                                     IconButton(
                                         onClick = {
                                             authViewModel.logout()
-                                            navController.navigate(Screen.Login.route){
-                                                popUpTo(navController.graph.findStartDestination().id){
-                                                    inclusive = true
-                                                }
+                                            navController.navigate(Screen.Login.route) {
+                                                popUpTo(navController.graph.findStartDestination().id) { inclusive = true }
                                                 launchSingleTop = true
                                             }
                                         }
                                     ) {
-                                        Icon(
-                                            imageVector = Icons.AutoMirrored.Filled.ExitToApp,
-                                            contentDescription = "Logout"
-                                        )
+                                        Icon(Icons.AutoMirrored.Filled.ExitToApp, contentDescription = "Logout")
                                     }
                                 }
                             )
                         }
                     },
                     bottomBar = {
-                        if (showBars){
+                        if (showBars) {
                             NavigationBar {
-                                val navBackStackEntry by navController.currentBackStackEntryAsState()
-                                val currentRoute = navBackStackEntry?.destination?.route.orEmpty()
-
-                                bottomScreen.forEach { screen ->
+                                val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route.orEmpty()
+                                bottomScreens.forEach { screen ->
                                     NavigationBarItem(
                                         icon = { screen.icon?.let { Icon(it, contentDescription = screen.label) } },
                                         label = { Text(screen.label) },
                                         selected = currentRoute.startsWith(screen.route.removeSuffix("/{barcode}")),
                                         onClick = {
                                             navController.navigate(screen.route) {
-                                                popUpTo(navController.graph.findStartDestination().id) {
-                                                    inclusive = false
-                                                }
+                                                popUpTo(navController.graph.findStartDestination().id) { inclusive = false }
                                                 launchSingleTop = true
                                                 restoreState = true
                                             }
@@ -127,50 +93,60 @@ class MainActivity : ComponentActivity() {
                 ) { innerPadding ->
                     NavHost(
                         navController = navController,
-                        startDestination = if (auth.currentUser != null) Screen.Home.route else Screen.Login.route,
+                        startDestination = "splash",
                         modifier = Modifier.padding(innerPadding)
                     ) {
-                        composable("login"){
+                        composable("splash") {
+                            val currentUser = auth.currentUser
+                            LaunchedEffect(Unit) {
+                                val destination = if (currentUser != null) Screen.Home.route else Screen.Login.route
+                                navController.navigate(destination) {
+                                    popUpTo("splash") { inclusive = true }
+                                }
+                            }
+                            Column(
+                                modifier = Modifier.fillMaxSize(),
+                                verticalArrangement = Arrangement.Center,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) { CircularProgressIndicator() }
+                        }
+
+                        composable("login") {
                             LoginScreen(
                                 onLoginSuccess = { navController.navigate(Screen.Home.route) },
                                 onNavigateToRegister = { navController.navigate(Screen.Register.route) }
                             )
                         }
 
-                        composable("register"){
+                        composable("register") {
                             RegisterScreen(
                                 onRegisterSuccess = { navController.navigate(Screen.Home.route) },
                                 onNavigateToLogin = { navController.navigate(Screen.Login.route) }
                             )
                         }
 
-                        composable(Screen.Home.route){
-                            HomeScreen()
+                        composable(Screen.Home.route) { HomeScreen() }
+
+                        composable(Screen.Scan.route) {
+                            ProductScanScreen { barcode ->
+                                navController.navigate(Screen.Result.createRoute(barcode))
+                            }
                         }
 
-                        composable(Screen.Scan.route){
-                            ProductScanScreen(onBarcodeScanned = { barcode ->
+                        composable(Screen.Input.route) {
+                            ProductInputScreen { barcode ->
                                 navController.navigate(Screen.Result.createRoute(barcode))
-                            })
-                        }
-
-                        composable(Screen.Input.route){
-                            ProductInputScreen(onSubmit = { barcode ->
-                                navController.navigate(Screen.Result.createRoute(barcode))
-                            })
+                            }
                         }
 
                         composable(
                             route = Screen.Result.route,
-                            arguments = listOf(navArgument("barcode") {
-                                type = NavType.StringType
-                            })
+                            arguments = listOf(navArgument("barcode") { type = NavType.StringType })
                         ) { backStackEntry ->
-                            val barcode = backStackEntry.arguments?.getString("barcode") ?: ""
-                            ProductResultScreen(barcode = barcode)
+                            ProductResultScreen(backStackEntry.arguments?.getString("barcode").orEmpty())
                         }
 
-                        composable(Screen.Scanned.route){
+                        composable(Screen.Scanned.route) {
                             ScannedProductScreen(navController = navController)
                         }
                     }
--- app/src/main/java/com/example/nutralis/ui/auth/LoginScreen.kt
+++ app/src/main/java/com/example/nutralis/ui/auth/LoginScreen.kt
@@ -1,22 +1,39 @@
 package com.example.nutralis.ui.auth
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
@@ -31,42 +48,144 @@ fun LoginScreen(
     var password by remember { mutableStateOf("") }
 
     Column(
-        modifier = Modifier.padding(16.dp)
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(
+                        Color(0xFFEBC894),
+                        Color(0xFFB49EF4)
+                    ),
+                    start = Offset(0f, 0f),
+                    end = Offset(1000f, 1000f)
+                )
+            )
+            .padding(horizontal = 16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        OutlinedTextField(
-            value = email,
-            onValueChange = {email = it},
-            label = { Text("Email") }
-        )
-        OutlinedTextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Password") },
-            visualTransformation = PasswordVisualTransformation()
-        )
-
-        Button(
-            onClick = { viewModel.login(email, password) },
-            enabled = !state.isLoading,
-            modifier = Modifier.padding(16.dp)
+        Column (
+            modifier = Modifier
+                .clip(RoundedCornerShape(16.dp))
+                .background(Color.White.copy(alpha = 0.6f))
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Text("Login")
-        }
+            Text(
+                "Login",
+                color = Color.Black,
+                fontSize = 40.sp,
+                fontWeight = FontWeight.Bold,
+            )
+            Text(
+                "Enter your email and password to login",
+                color = Color.Gray,
+                fontSize = 13.sp,
+                modifier = Modifier.padding(vertical = 16.dp)
+            )
+            TextField(
+                value = email,
+                onValueChange = {email = it},
+                label = { Text("Email") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        top = 8.dp,
+                        bottom = 4.dp
+                    )
+                    .defaultMinSize(minHeight = 36.dp),
+                shape = RoundedCornerShape(16.dp),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.White,
+                    unfocusedContainerColor = Color.White,
+                    focusedLabelColor = Color.Gray,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent,
+                    errorIndicatorColor = Color.Transparent
+                ),
+                textStyle = TextStyle(
+                    color = Color.Black,
+                    fontSize = 13.sp
+                )
+            )
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        top = 4.dp,
+                        bottom = 8.dp
+                    )
+                    .defaultMinSize(minHeight = 36.dp),
+                shape = RoundedCornerShape(16.dp),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.White,
+                    unfocusedContainerColor = Color.White,
+                    focusedLabelColor = Color.Gray,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent,
+                    errorIndicatorColor = Color.Transparent
+                ),
+                textStyle = TextStyle(
+                    color = Color.Black,
+                    fontSize = 13.sp
+                )
+            )
 
-        TextButton(onClick = onNavigateToRegister) {
-            Text("No account? Register here")
-        }
+            Button(
+                onClick = { viewModel.login(email, password) },
+                enabled = !state.isLoading,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF1D61E7),
+                    contentColor = Color.White
+                ),
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 16.dp)
+                    .height(56.dp)
+            ) {
+                if(state.isLoading){
+                    CircularProgressIndicator()
+                } else {
+                    Text(
+                        "Login",
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
 
-        state.error?.let {
-            Text(it, color = Color.Red)
-        }
+            Row (
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    "No account?",
+                    color = Color.Black,
+                    fontSize = 13.sp
+                )
+                TextButton(onClick = onNavigateToRegister) {
+                    Text(
+                        "Register here",
+                        fontSize = 13.sp,
+                        color = Color(0xFF1D61E7)
+                    )
+                }
+            }
 
-        if(state.isLoading){
-            CircularProgressIndicator()
-        }
+            state.error?.let {
+                Text(it, color = Color.Red)
+            }
 
-        if(state.isSuccess){
-            onLoginSuccess()
+            if(state.isSuccess){
+                onLoginSuccess()
+            }
         }
     }
 }
