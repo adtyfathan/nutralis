f1c835c38b45807bae009e6b0609da04e645f83f
senntz <firmansyahfikri456@gmail.com>
2025-09-30 22:14:26 +0700

    memperbarui halaman histori

--- app/src/main/java/com/ofa/nutralis/ui/product/ScannedProductScreen.kt
+++ app/src/main/java/com/ofa/nutralis/ui/product/ScannedProductScreen.kt
@@ -1,21 +1,34 @@
 package com.ofa.nutralis.ui.product
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.ofa.nutralis.R
 import com.ofa.nutralis.navigation.Screen
 
 @Composable
@@ -39,7 +52,27 @@ fun ScannedProductScreen(
                 CircularProgressIndicator()
             }
             products.isEmpty() -> {
-                Text("Belum ada produk")
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.permission),
+                        contentDescription = "Empty State",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.size(120.dp).clip(CircleShape)
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        "Belum ada produk",
+                        fontWeight = FontWeight.Bold,
+                    )
+                    Text(
+                        "Scan produk untuk menambahkan histori anda"
+                    )
+                }
+
             }
             else -> {
                 LazyColumn(
